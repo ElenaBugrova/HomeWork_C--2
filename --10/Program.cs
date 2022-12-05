34dab6590a4439bddd214ec8f5787742f3d581b8
ElenaBugrova <evilisia@mail.ru>
2022-12-05 22:03:27 +0300

    Домашка_2

---
+++ --10/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное
+//число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int num=int.Parse(Console.ReadLine());
+int result = num %100/10;
+Console.WriteLine($"{num} -> {result}");
---
+++ --15/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+Console.Write("Введите число: ");
+int num=int.Parse(Console.ReadLine());
+if (num ==6 || num==7)
+{
+   Console.WriteLine($"{num} --> да");
+}
+else
+{
+    Console.WriteLine($"{num} --> нет");
+}
